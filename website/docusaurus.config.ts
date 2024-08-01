7141b469dfea8f6a21b4cdd0cad72d9f0112ee8a
imnaiyar <imamnaiyar29@gmail.com>
2024-08-01 02:44:02 -0500

    pwa

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -52,6 +52,34 @@ const config: Config = {
         },
       },
     ],
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+        ],
+      },
+    ]
   ],
 
 
